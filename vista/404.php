d1b20267494b80a1cbe794aae87fbfdc781baa8d
Alain <Alain@AMDFX>
2013-11-25 18:37:58 -0500

    corrigiendo 404

--- vista/404.php
+++ vista/404.php
@@ -3,20 +3,20 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Error 404</title>
-		<? if(!empty($redireccion)): ?>
+		<?php if(!empty($redireccion)): ?>
 		<meta http-equiv="refresh" content="2;url=<?=$redireccion?>">
-		<? endif; ?>
+		<?php endif; ?>
 		<style type="text/css">
 			h1,p{margin: 0;padding: 0;}
 			body{background: #a1cc4d;font-family: Tahoma, Geneva, sans-serif;}
 			h1{margin-bottom: 10px;}
-			#caja{background: #fff url(img/error-404.png) no-repeat 20px 30px;border:10px solid #89ae42;height: 140px;left: 50%;margin-left: -380px;margin-top: -100px;padding: 30px 30px 30px 170px;position: absolute;top: 50%;width: 500px;}
+			#caja{background: #fff url(<?php echo DIR_STATIC; ?>img/error-404.jpg) no-repeat 20px 30px;border:10px solid #89ae42;height: 140px;left: 50%;margin-left: -380px;margin-top: -100px;padding: 30px 30px 30px 170px;position: absolute;top: 50%;width: 500px;}
 		</style>
     </head>
     <body>
 		<div id="caja">
 			<h1>&iexcl;Ups! página no encontrada</h1>
-			<p><?=$mensaje?></p>
+			<p><?php echo $mensaje; ?></p>
 		</div>
     </body>
 </html>
